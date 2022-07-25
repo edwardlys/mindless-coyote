80184e632523f629f6427a6cf419458f2b1a71cc
Edward Lim <elys.1993a@gmail.com>
2022-07-25 21:54:12 +0800

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'map' }">Map</RouterLink>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
