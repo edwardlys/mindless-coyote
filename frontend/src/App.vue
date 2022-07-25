b162e8f7e080a3680ef09f04468b287a11fbc3fb
Edward Lim <elys.1993a@gmail.com>
2022-07-25 22:59:26 +0800

    Updated gps getter

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@ import { RouterLink, RouterView } from "vue-router";
     <nav>
       <RouterLink :to="{ name: 'home' }">Home</RouterLink>
       <RouterLink :to="{ name: 'map' }">Map</RouterLink>
+      <RouterLink :to="{ name: 'gps' }">Gps</RouterLink>
     </nav>
   </div>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import MapView from "../views/MapView.vue";
+import GpsView from "../views/GpsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/map",
       name: "map",
       component: MapView,
+    },
+    {
+      path: "/gps",
+      name: "gps",
+      component: GpsView,
     },
   ],
 });
